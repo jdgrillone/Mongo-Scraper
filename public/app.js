8a4d26374f768c92c496caf7e7f76bf387dc4914
jdgrillone <jdgrillone@gmail.com>
2018-02-04 12:32:27 -0700

    Comments now populate correctly/Added placeholders for input form

--- public/app.js
+++ public/app.js
@@ -25,19 +25,22 @@ $(document).on("click", ".article-click", function () {
             console.log(data);
             // The title of the article
             $("#notes").append("<h5>" + data.title + "</h5>");
+            // Create a div for notes to go into
+            $("#notes").append("<div id='notes-list'></div>");
             // An input to enter a new title
-            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<input placeholder='Comment Title' id='titleinput' name='title' >");
             // A textarea to add a new note body
-            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<textarea id='bodyinput' placeholder='Comment Body' name='body'></textarea>");
             // A button to submit a new note, with the id of the article saved to it
             $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
             // If there's a note in the article
             if (data.note) {
+                $("#notes-list").append("<h6>" + data.note.title + "</h6> <p>" + data.note.body + "</p>")
                 // Place the title of the note in the title input
-                $("#titleinput").val(data.note.title);
+                // $("#titleinput").val(data.note.title);
                 // Place the body of the note in the body textarea
-                $("#bodyinput").val(data.note.body);
+                // $("#bodyinput").val(data.note.body);
             }
         });
 });
